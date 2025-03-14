5e5eb25ef84802d3bd6140f9b0e005173c9bb801
kailai-13 <kailainathan2006@gmail.com>
2025-03-14 21:20:15 +0530

    nothing

--- app.py
+++ app.py
@@ -658,4 +658,4 @@ def download_student_attendance():
     )
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000 ,ssl_context=("cert.pem", "key.pem"))
+    app.run(host="0.0.0.0",port=5000 ,ssl_context=("cert.pem", "key.pem"), debug=True)
