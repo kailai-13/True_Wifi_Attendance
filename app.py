bc1d78d75eceb1570a6d98d3dfe2b860f0e1a83b
kailai-13 <kailainathan2006@gmail.com>
2025-04-07 16:11:41 +0530

    i am a mental

--- app.py
+++ app.py
@@ -1081,6 +1081,6 @@ def get_admin_rooms():
         })
     
     return jsonify({'success': True, 'rooms': room_list})
-    
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000 ,ssl_context=("cert.pem", "key.pem"),debug=True)
